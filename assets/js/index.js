7014e729499bc176d63e1fd7ac7adc2f31e8f6b3
Ryan Hoshor <ryan@hoshor.me>
2018-09-04 21:42:52 -0500

    Compatibility with Ghost v2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,53 +2,34 @@
  * Main JS file for Jasper behaviours
  */
 
-/*globals jQuery, document */
+if (typeof jQuery == 'undefined') {
+    document.write('<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></' + 'script>');
+}
+
 (function ($) {
     "use strict";
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
+    // Header Parallax and Fade
+    $(window).on('scroll', function() {
+    
+        var scroll_top = $(this).scrollTop();
+        var top_offset = 600;
+        
+        $('#site-head-content').css({
+            'opacity' : (1-scroll_top/top_offset),
+            'top' : (scroll_top*-.2)
+        });
+        
+        /*
+        // Background Image Parallax
+        $('#site-head').css({
+            'background-position' : 'center ' + (scroll_top*-.07) + 'px'
+        });
+        */
+    
+    });
 
-        //$("#blog-logo").prependTo("#site-head-content");
-        
-        // Header Parallax and Fade
-		$(window).on('scroll', function() {
-		
-			var scroll_top = $(this).scrollTop();
-			var top_offset = 600;
-			
-			$('#site-head-content').css({
-				'opacity' : (1-scroll_top/top_offset),
-				'top' : (scroll_top*-.2)
-			});
-			
-/*
-			// Background Image Parallax
-			$('#site-head').css({
-				'background-position' : 'center ' + (scroll_top*-.07) + 'px'
-			});
-*/
-		
-		});
-		    
-        
-        // Disqus Comments
-        
-        $('.showDisqus').on('click', function(){   // click event of the show comments button
-			var disqus_shortname = 'shilohstream';
-			//var disqus_identifier = '{{url absolute="true"}}'; // needs to be unique...
-
-			$.ajax({
-		         type: "GET",
-		         url: "http://" + disqus_shortname + ".disqus.com/embed.js",
-		         dataType: "script",
-		         cache: true
-		    });
-	
-		    $(this).fadeOut();  // remove the show comments button
-		});
-        
-        // Shameless self-promotion
-        console.log("Yo, fellow dev. Check out our homepage, and find out about our other goodies: http://www.farmsoftstudios.com")
+    // Shameless self-promotion
+    console.log("Yo, fellow dev. Check out our homepage, and find out about our other goodies: http://www.farmsoftstudios.com")
 
 }(jQuery));
